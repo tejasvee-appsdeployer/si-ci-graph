5afb36cb54b435897f26e1a553c434fc46aaa6a8
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-12-29 18:38:32 +0530

    Added Bootstrap and made SI and CI calculation logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [principal, setPrincipal] = useState(0);
+	const [rate, setRate] = useState(0);
+	const [duration, setDuration] = useState(0);
+	const [simpleInterest, setSimpleInterest] = useState(0);
+	const [compoundInterest, setCompoundInterest] = useState(0);
+
+	const calculateSimpleInterest = () => {
+		setSimpleInterest((principal * rate * duration) / 100);
+	};
+
+	const calculateCompoundInterest = () => {
+		setCompoundInterest(principal * (1 + rate / 100) ** duration - principal);
+	};
+
+	return (
+		<div>
+			<h1>Simple Interest Calculator</h1>
+			<label>
+				Principal:
+				<input
+					type="number"
+					value={principal}
+					onChange={(e) => setPrincipal(e.target.value)}
+				/>
+			</label>
+			<br />
+			<label>
+				Rate:
+				<input
+					type="number"
+					value={rate}
+					onChange={(e) => setRate(e.target.value)}
+				/>
+			</label>
+			<br />
+			<label>
+				Duration:
+				<input
+					type="number"
+					value={duration}
+					onChange={(e) => setDuration(e.target.value)}
+				/>
+			</label>
+			<br />
+			<button onClick={calculateSimpleInterest}>
+				Calculate Simple Interest
+			</button>
+			<br />
+			<button onClick={calculateCompoundInterest}>
+				Calculate Compound Interest
+			</button>
+			<br />
+			<h2>Simple Interest: {simpleInterest}</h2>
+			<h2>Compound Interest: {compoundInterest}</h2>
+		</div>
+	);
+};
 
 export default App;
